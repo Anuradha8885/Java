d58b8ebfb4944fe8f55069dddd10ff69c8fc6df7
Anuradha8885 <anuradhatalekar137@gmail.com>
2025-04-11 09:28:41 +0530

    Create Main.java

---
+++ Date&Time/Main.java
@@ -0,0 +1,26 @@
+/*import java.time.LocalDate;
+
+public class main{
+    public static void main(String[] agrs){
+        LocalDate myObj= LocalDate.now();
+        System.out.println(myObj);
+    }
+}
+
+import java.time.LocalTime;
+
+public class main{
+    public static void main (String[] agrs) {
+        LocalTime myObj= LocalTime.now();
+        System.out.println(myObj);
+    }
+}*/
+
+import java.time.LocalDateTime;
+
+public class Main{
+    public static void main(String[] agrs){
+        LocalDateTime myObj= LocalDateTime.now();
+        System.out.println(myObj);
+    }
+}
